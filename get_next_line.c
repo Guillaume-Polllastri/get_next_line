5cfc73d729d5222c3ebd97cf769c017a7032ca86
Guillaume <guillox01@gmail.com>
2025-05-07 20:13:39 +0200

    gnl debut

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,24 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 17:24:01 by gpollast          #+#    #+#             */
-/*   Updated: 2025/05/07 18:40:07 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/05/07 20:13:18 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	*get_line()
+int	*get_line(char *buffer)
 {
-	
+	if (ft_strchr(buffer, '\n') == NULL)
+		return (1);
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	read(fd, buffer[], BUFFER_SIZE);
+	void	*buffer;
+	char	*tmp;
+
+	if (read(fd, buffer[], BUFFER_SIZE) == 0 || get_line(buffer) == 1)
+		return (le resultat);
 }
