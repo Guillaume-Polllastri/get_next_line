bdb2424fe5077b3e6e25b2836c37f9afd52e0f5f
Guillaume <guillox01@gmail.com>
2025-05-07 17:23:18 +0200

    gnl.h

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 17:19:59 by gpollast          #+#    #+#             */
+/*   Updated: 2025/05/07 17:22:15 by gpollast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
