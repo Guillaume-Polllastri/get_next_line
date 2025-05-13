1518fda62d1ecb28176ff22d21c57bd20d563b2b
Guillaume Pollastri <gpollast@f6r7s5.clusters.42paris.fr>
2025-05-13 11:23:44 +0200

    define buffer-size

--- get_next_line.h
+++ get_next_line.h
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: guill <guill@student.42.fr>                +#+  +:+       +#+        */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 17:19:59 by gpollast          #+#    #+#             */
-/*   Updated: 2025/05/09 15:48:49 by guill            ###   ########.fr       */
+/*   Updated: 2025/05/13 11:22:57 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 
 # include <unistd.h>
 # include <stdlib.h>
